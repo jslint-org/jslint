d274eaad4650deabd95843c97064ae917f689380
Jeremy Kahn <jeremyckahn@gmail.com>
2011-02-19 16:36:20 -0600

    Added some echos to build.sh, and renamed it because for some reason I saved it as build.js before.

--- build.js
+++
@@ -1,4 +0,0 @@
-# Get the latest source from Douglas Crockford's supporting Github repos and build fullwebjslint.js
-
-git submodule update
-cat JSON-js/json2.js  fulljslint.js ADsafe/adsafe.js intercept.js > fullwebjslint.js
