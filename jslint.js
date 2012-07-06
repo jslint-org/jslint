9247ef5cfb09d56faa62f8c3bb68a932fbc08995
Kevin Locke <kevin@kevinlocke.name>
2012-07-05 22:12:57 -0600

    Allow get/set to be specified independently in ES5 object literals
    Allow property getters and setters to be specified independently.  The
    primary objective is to allow read-only properties.  This implementation
    also allows code which may or may not not be desirable, such as
    write-only properties and reversed and/or separated getter/setter
    declarations.  These may be addressed in subsequent commits.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- jslint.js
+++ jslint.js
@@ -3856,15 +3856,12 @@ klass:              do {
 
 
     prefix('{', function () {
-        var get, i, j, name, p, set, seen = {};
+        var get, i, name, p, set, seenget = {}, seenset = {};
         this.arity = 'prefix';
         this.first = [];
         step_in();
         while (next_token.id !== '}') {
             indent.wrap = false;
-
-// JSLint recognizes the ES5 extension for get/set in object literals,
-// but requires that they be used in pairs.
 
             edge();
             if (next_token.string === 'get' && peek().id !== ':') {
@@ -3888,17 +3885,24 @@ klass:              do {
                 if (p && p.length) {
                     warn('parameter_a_get_b', p[0], p[0].string, i);
                 }
-                comma();
-                set = next_token;
-                spaces();
-                edge();
-                advance('set');
-                set.string = '';
-                one_space_only();
-                j = property_name();
-                if (i !== j) {
-                    stop('expected_a_b', token, i, j || next_token.string);
+                name.first = get;
+                if (seenget[i] === true) {
+                    warn('duplicate_a', next_token, i);
                 }
+                seenget[i] = true;
+            } else if (next_token.string === 'set' && peek().id !== ':') {
+                if (!option.es5) {
+                    warn('es5');
+                }
+                set = next_token;
+                advance('set');
+                one_space_only();
+                name = next_token;
+                i = property_name();
+                if (!i) {
+                    stop('missing_property');
+                }
+                set.string = '';
                 do_function(set);
                 if (set.block.length === 0) {
                     warn('missing_a', token, 'throw');
@@ -3909,7 +3913,11 @@ klass:              do {
                 } else if (p[0].string !== 'value') {
                     stop('expected_a_b', p[0], 'value', p[0].string);
                 }
-                name.first = [get, set];
+                name.first = set;
+                if (seenset[i] === true) {
+                    warn('duplicate_a', next_token, i);
+                }
+                seenset[i] = true;
             } else {
                 name = next_token;
                 i = property_name();
@@ -3919,12 +3927,12 @@ klass:              do {
                 advance(':');
                 spaces();
                 name.first = expression(10);
+                if (seenget[i] === true || seenset[i] === true) {
+                    warn('duplicate_a', next_token, i);
+                }
+                seenget[i] = seenget[i] = true;
             }
             this.first.push(name);
-            if (seen[i] === true) {
-                warn('duplicate_a', next_token, i);
-            }
-            seen[i] = true;
             tally_property(i);
             if (next_token.id !== ',') {
                 break;
