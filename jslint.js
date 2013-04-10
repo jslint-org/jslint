1f1820909def962e6248c59596040daa591302e8
Douglas Crockford <douglascrockford@github.com>
2013-04-10 10:20:52 -0700

    - 1

--- jslint.js
+++ jslint.js
@@ -1,5 +1,5 @@
 // jslint.js
-// 2013-04-06
+// 2013-04-09
 
 // Copyright (c) 2002 Douglas Crockford  (www.JSLint.com)
 
@@ -986,7 +986,7 @@ var JSLINT = (function () {
                 while (pos >= source_row.length) {
                     pos = 0;
                     if (!next_line()) {
-                        stop_at('unclosed', line, from);
+                        stop_at('unclosed', line - 1, from);
                     }
                 }
                 c = source_row.charAt(pos);
@@ -4503,7 +4503,7 @@ klass:              do {
 
     itself.jslint = itself;
 
-    itself.edition = '2013-04-06';
+    itself.edition = '2013-04-09';
 
     return itself;
 }());
