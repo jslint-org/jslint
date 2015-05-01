557afd32bcaa35480d31a86f02d3a8c06a4e5b5c
Douglas Crockford <douglas@crockford.com>
2015-05-01 04:22:31 -0700

    BETA

---
+++ browser.js
@@ -0,0 +1,142 @@
+// browser.js
+// 2015-03-03
+// Copyright (c) 2015 Douglas Crockford  (www.JSLint.com)
+
+/*global 
+    ADSAFE, document, REPORT, jslint 
+*/
+
+/*property
+    ___nodes___, check, create, each, enable, error, focus, function, getName,
+    getTitle, getValue, innerHTML, length, lib, on, property, q, select, split,
+    style, value
+*/
+
+// This is the web script companion file for JSLint. It includes code for
+// interacting with the browser and generating the reports.
+
+ADSAFE.lib("browser_ui", function () {
+    'use strict';
+    
+    var rx_separator = /[\s,;'"]+/;
+    
+    function setHTML(bunch, html) {
+        bunch.___nodes___[0].innerHTML = html;
+    }
+
+    return function (dom) {
+        
+// This function is the entry point to this web module.
+
+// First get handles to some of the page features.        
+        
+        var warnings = dom.q('#JSLINT_WARNINGS'),
+            warnings_div = warnings.q('>div'),
+            options = dom.q('#JSLINT_OPTIONS'),
+            global = dom.q('#JSLINT_GLOBAL'),
+            property = dom.q('#JSLINT_PROPERTY'),
+            property_textarea = property.q('>textarea'),
+            report_field = dom.q('#JSLINT_REPORT'),
+            report_div = report_field.q('>div'),
+            select = dom.q('#JSLINT_SELECT'),
+            source = dom.q('#JSLINT_SOURCE'),
+            source_textarea = source.q('>textarea'),
+            aux = dom.q('#JSLINT_AUX');
+
+        function clear() {
+            warnings.style('display', 'none');
+            report_field.style('display', 'none');
+            property.style('display', 'none');
+            aux.style('display', 'none');
+            warnings_div.value('');
+            report_div.value('');
+            property_textarea.value('');
+            source_textarea.value('');
+            source_textarea.focus();
+        }
+        
+        function clear_options() {
+            options.q('input_checkbox').check(false);
+            options.q('input_text').value('');
+            global.value('');
+        }
+
+        function call_jslint() {
+            
+// First build the option object.            
+            
+            var option = Object.create(null);
+            options.q('input_checkbox:checked').each(function (node) {
+                option[node.getTitle()] = true;
+            });
+            options.q('input_text').each(function (node) {
+                var value = +node.getValue();
+                if (isFinite(value) && value > 0) {
+                    option[node.getTitle()] = value;
+                }
+            });
+            
+// Call JSLint with the source text, the options, and the predefined globals.          
+            
+            var source_string = source_textarea.getValue();
+            var global_string = global.getValue();
+            var result = jslint(
+                source_string, 
+                option, 
+                global_string === '' 
+                ? undefined
+                : global_string.split(rx_separator)
+            );
+            
+// Generate the reports.
+
+            var error_html = REPORT.error(result);
+            var function_html = REPORT.function(result);
+            var property_text = REPORT.property(result);
+
+// Display the reports.
+
+            setHTML(warnings_div, error_html);
+            warnings.style('display', error_html.length === 0 
+            ? 'none' 
+            : 'block');
+            setHTML(report_div, function_html);
+            report_field.style('display', 'block');
+            if (property_text) {
+                property_textarea.value(property_text);
+                property.style('display', 'block');
+                select.enable(true);
+            } else {
+                property.style('display', 'none');
+                select.enable(false);
+            }
+            aux.style('display', 'block');
+            source_textarea.select();
+        }
+        
+        function select_property_directive() {
+            property_textarea.select();
+        }
+        
+// Lay in the click handlers.        
+
+        dom.q('button').each(function (button) {
+            switch (button.getName()) {
+            case 'JSLint': 
+                button.on('click', call_jslint);
+                break;
+            case 'clear':
+                button.on('click', clear);
+                break;
+            case 'options':
+                button.on('click', clear_options);
+                break;
+            case 'select':
+                button.on('click', select_property_directive);
+                break;
+            }
+        });
+        source_textarea.select();
+    };
+});
+
---
+++ report.js
@@ -0,0 +1,201 @@
+// report.js
+// 2015-04-17
+// Copyright (c) 2015 Douglas Crockford  (www.JSLint.com)
+
+// Generate JSLint HTML reports.
+
+/*property
+    closure, column, context, edition, error, filter, forEach, fudge, function,
+    functions, global, id, imports, isArray, join, json, keys, length, level,
+    line, lines, message, module, name, names, option, parameters, property, 
+    push, replace, role, signature, sort, stop, warnings
+*/
+
+var REPORT = (function () {
+    'use strict';
+
+    var rx_amp = /&/g,
+        rx_gt = />/g,
+        rx_lt = /</g;
+    
+    function entityify(string) {
+        
+// Replace & < > with less destructive entities.        
+    
+        return String(string)
+                .replace(rx_amp, '&amp;')
+                .replace(rx_lt, '&lt;')
+                .replace(rx_gt, '&gt;');
+    }
+    
+    return {
+
+        error: function error_report(data) {
+
+// Produce the HTML Error Report.        
+    
+            var fudge = data.option.fudge 
+                ? 1 
+                : 0,
+                output = [];
+            if (data.stop) {
+                output.push("<center>J<u>SLint</u> was unable to finish.</center>");
+            }
+            data.warnings.forEach(function (warning) {
+                output.push(
+                    "<cite><address>line ", 
+                    entityify(warning.line + fudge),
+                    " column ", 
+                    entityify(warning.column + fudge),
+                    "</address>",
+                    entityify(warning.message),
+                    "</cite><samp>",
+                    entityify(data.lines[warning.line] || ''),
+                    "</samp>"
+                );
+            });
+            return output.join("");
+        },
+
+        function: function function_report(data) {
+    
+// Produce the HTML Function Report.        
+    
+            var fudge = data.option.fudge 
+                ? 1 
+                : 0, 
+                mode = data.module
+                ? "module"
+                : "global",
+                output = [];
+
+            if (data.json) {
+                return data.warnings.length === 0
+                ? "<center>JSON: good.</center>"
+                : "<center>JSON: bad.</center>";
+            } 
+            
+            function detail(title, array) {
+                if (Array.isArray(array) && array.length > 0) {
+                    output.push(
+                        "<dt>", 
+                        entityify(title),
+                        "</dt><dd>",
+                        array.join(", "),
+                        "</dd>"
+                    );
+                }
+            }
+
+            if (data.functions.length === 0) {
+                output.push("<center>There are no functions.</center>");
+            }
+            var global = Object.keys(data.global.context).sort(),
+                imports = data.imports.sort();
+            if (global.length + imports.length > 0) {
+                output.push("<dl class=level0>");
+                detail(mode, global);
+                detail("imports", imports);
+                output.push("</dl>");
+            }
+            
+            if (data.functions.length > 0) {
+                data.functions.forEach(function (the_function) {
+                    var context = the_function.context,
+                        list = Object.keys(context);
+                    output.push(
+                        "<dl class=level", 
+                        entityify(the_function.level),
+                        "><address>line ", 
+                        entityify(the_function.line + fudge),
+                        "</address><dfn>", 
+                        (
+                            the_function.name === "=>"
+                            ? entityify(the_function.signature) + " =>"
+                            : typeof the_function.name === 'string'
+                            ? '«' + entityify(the_function.name) + '»'
+                            : "<b>" + entityify(the_function.name.id) + "</b>"
+                        ) + entityify(the_function.signature),
+                        "</dfn>"
+                    );
+                    if (Array.isArray(the_function.parameters)) {
+                        var params = [];
+                        the_function.parameters.forEach(function extract(name) {
+                            if (name.id === '{' || name.id === '[') {
+                                name.names.forEach(extract);
+                            } else {
+                                if (name.id !== 'ignore') {
+                                    params.push(name.id);
+                                }
+                            }
+                        });
+                        detail(
+                            "parameter", 
+                            params.sort()
+                        );
+                    }
+                    list.sort();
+                    detail("variable", list.filter(function (id) {
+                        var the_variable = context[id];
+                        return the_variable.role === 'variable' && 
+                                the_variable.function === the_function;
+                    }));
+                    detail("exception", list.filter(function (id) {
+                        return context[id].role === 'exception';
+                    }));
+                    detail("closure", list.filter(function (id) {
+                        var the_variable = context[id];
+                        return the_variable.closure === true &&
+                                the_variable.function === the_function;
+                    }));
+                    detail("outer", list.filter(function (id) {
+                        var the_variable = context[id];
+                        return the_variable.function !== the_function &&
+                                the_variable.function.id !== '(global)';
+                    }));
+                    detail(mode, list.filter(function (id) {
+                        return context[id].function.id === '(global)';
+                    }));
+                    detail("label", list.filter(function (id) {
+                        return context[id].role === 'label';
+                    }));
+                    output.push("</dl>");
+                });
+            }
+            output.push(
+                "<center>J<u>SLint</u> edition ", 
+                entityify(data.edition), 
+                "</center>"
+            );
+            return output.join("");
+        },
+
+        property: function property_directive(data) {
+    
+// Produce the /*property*/ directive.        
+    
+            var not_first = false,
+                output = ["/*property"],
+                length = 1111,
+                properties = Object.keys(data.property);
+                
+            if (properties.length > 0) {        
+                properties.sort().forEach(function (key) {
+                    if (not_first) {
+                        output.push(",");
+                        length += 2;
+                    }
+                    not_first = true;
+                    if (length + key.length > 78) {
+                        length = 4;
+                        output.push("\n   ");
+                    }
+                    output.push(" ", key);
+                    length += key.length;
+                });
+                output.push("\n*/\n");
+                return output.join('');
+            }
+        }
+    };
+}());
