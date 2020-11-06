bca8b225a376352899d634442802b241fee8b97b
Douglas Crockford <262641+douglascrockford@users.noreply.github.com>
2020-11-06 09:58:13 -0800

    vestigial

--- jslint.js
+++ jslint.js
@@ -1,5 +1,5 @@
 // jslint.js
-// 2020-10-21
+// 2020-11-06
 // Copyright (c) 2015 Douglas Crockford  (www.JSLint.com)
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -4834,21 +4834,6 @@ function whitage() {
                     ) {
                         one_space_only();
                     } else if (
-                        left.id === "var"
-                        || left.id === "const"
-                        || left.id === "let"
-                    ) {
-                        push();
-                        closer = ";";
-                        free = false;
-                        open = left.open;
-                        if (open) {
-                            margin = margin + 4;
-                            at_margin(0);
-                        } else {
-                            one_space_only();
-                        }
-                    } else if (
 
 // There is a space between left and right.
 
@@ -5006,7 +4991,7 @@ export default Object.freeze(function jslint(
     }
     return {
         directives,
-        edition: "2020-10-21",
+        edition: "2020-11-06",
         exports,
         froms,
         functions,
