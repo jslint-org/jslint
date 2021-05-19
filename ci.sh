363acc94a097442d0ee94e56b720d08d3cdfc1be
Kai Zhu <kaizhu256@gmail.com>
2021-05-19 16:13:15 -0500

    - add files .gitconfig, Daley-Bold.woff2, Programma-Bold.woff2, icon-folder-open-solid.svg, icon-window-maximize-regular.svg - ci - fix http-links after moving to jslint-org

--- ci.sh
+++ ci.sh
@@ -218,15 +218,8 @@ shGithubCi() {(set -e
     shJslintCli .
     # create coverage-report
     shRunWithCoverage shJslintCli jslint.js
-    # screenshot coverage-report
-    shBrowserScreenshot .build/coverage/jslint.js.html
     # screenshot live-web-demo
     shBrowserScreenshot https://jslint.com/index.html
-    if [ -f .build/screenshot.browser.%2Findex.html.png ]
-    then
-        mv .build/screenshot.browser.%2Findex.html.png \
-            .build/screenshot.browser.%2Fjslint.com%2Findex.html.png
-    fi
 )}
 
 shGithubArtifactUpload() {(set -e
@@ -633,7 +626,9 @@ shReadmeLinkValidate() {(set -e
             /[\u0022\u0027]/g
         ), "").replace((
             /\/branch\.\w+?\//g
-        ), "/branch.alpha/");
+        ), "/branch.alpha/").replace((
+            /\bjslint-org\/jslint\b/g
+        ), process.env.GITHUB_REPOSITORY);
         // ignore private-link
         if (
             process.env.npm_package_private &&
