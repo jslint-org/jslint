8616cb4ef9d8b3007222648559d086621274f54b
kai zhu <kaizhu256@gmail.com>
2021-06-30 21:37:04 -0500

    - license - change license to public-domain/unlicense. (#332)

--- jslint.mjs
+++ jslint.mjs
@@ -1,26 +1,32 @@
 #!/usr/bin/env node
-// jslint.mjs
-// Copyright (c) 2015 Douglas Crockford  (www.JSLint.com)
+// JSLint
+// Original Author: Douglas Crockford (https://www.jslint.com).
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// This is free and unencumbered software released into the public domain.
 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
 
-// The Software shall be used for Good, not Evil.
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+// For more information, please refer to <http://unlicense.org/>
+
 
 // jslint(source, option_dict, global_list) is a function that takes 3
 // arguments. The second two arguments are optional.
@@ -97,7 +103,7 @@
     last_statement,
     main, mode_force, mode_noop,
     process_exit,
-    toString,
+    toString, trimRight,
     variable,
     execArgv, fileURLToPath, filter, meta, order, reduce, stringify, token, url,
     a, all, allowed_option, argv, arity, artifact, assign, async, b,
@@ -7234,13 +7240,13 @@ function jslint(
         stack_trace = ""
     }, ii, list) {
         list[ii].formatted_message = String(
-            String(ii + 1).padStart(3, " ")
+            String(ii + 1).padStart(2, " ")
             + ". \u001b[31m" + message + "\u001b[39m"
             + " \u001b[90m\/\/ line " + line + ", column " + column
             + "\u001b[39m\n"
             + ("    " + line_source.trim()).slice(0, 72) + "\n"
             + stack_trace
-        ).trim();
+        ).trimRight();
     });
 
     return {
