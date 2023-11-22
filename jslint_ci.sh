4242069ab80e0d09efd7b4e48c54fc6567c92406
Kai Zhu <kaizhu256@gmail.com>
2023-11-22 12:51:16 -0600

    - ci - bugfix - Fix google-chrome unable to create screenshot because user-data-dir is /dev/null.

--- jslint_ci.sh
+++ jslint_ci.sh
@@ -204,6 +204,7 @@ shBrowserScreenshot() {(set -e
 # window-size $2
     node --input-type=module --eval '
 import moduleChildProcess from "child_process";
+import moduleOs from "os";
 import modulePath from "path";
 import moduleUrl from "url";
 // init debugInline
@@ -257,7 +258,7 @@ import moduleUrl from "url";
             "--incognito",
             "--screenshot",
             "--timeout=30000",
-            "--user-data-dir=/dev/null",
+            "--user-data-dir=" + moduleOs.tmpdir(),
             "--window-size=800x600",
             "-screenshot=" + file,
             (
