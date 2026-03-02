2c8776732bae906c043b740e106c8d47c931fdad
kai zhu <280571+kaizhu256@users.noreply.github.com>
2026-03-02 11:16:26 -0600

    # v2026.2.28 (#488)
    - jslint - Update warning to suggest Boolean(...) instead of !! for ternary-expression.
    - ci - Update shell-function shCiBase() to check npm-version-support, before running npm-pkg-fix.

--- .ci.sh
+++ .ci.sh
@@ -446,7 +446,7 @@ import moduleFs from "fs";
                     "type": "git",
                     "url": "https://github.com/jslint-org/jslint.git"
                 },
-                "version": "2026.2.1"
+                "version": "2026.2.28"
             }, undefined, 4)
         }
     ].map(async function ({
